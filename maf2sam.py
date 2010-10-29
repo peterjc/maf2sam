a17081b518d2e89f396c516db2a51721e4043583
peterjc <p.j.a.cock@googlemail.com>
2010-10-29 13:53:46 +0100

    Original rough v002 file, use object for each read

--- maf2sam.py
+++ maf2sam.py
@@ -1,10 +1,74 @@
 #v000 - Uses the gapped co-ordindates
 #v001 - Use the ungapped co-ordindates
+#v002 - Use object for each read
+#TODO Fill in pair partner info
 from Bio.Seq import reverse_complement
 from Bio import SeqIO
 
 ref = "AA14X_out.unpadded.fasta"
 maf = "AA14X_out.maf"
+
+class Read(object):
+    def __init__(self, contig_name,
+                 read_name="", template_name="", read_seq="", first_in_pair=True,
+                 ref_rc = False, ref_pos=0, map_qual=255, insert_size = 0,
+                 vect_left = 0, vect_right = 0,
+                 qual_left = 0, qual_right = 0,
+                 clip_left = 0, clip_right = 0,
+                 tags=""):
+        self.contig_name = contig_name
+        self.read_name = read_name
+        self.template_name = template_name
+        self.read_seq = read_seq
+        self.first_in_pair = first_in_pair
+        self.ref_rc = ref_rc
+        self.ref_pos = ref_pos
+        self.map_qual = map_qual
+        self.cigar = ""
+        self.insert_size = insert_size
+        self.vect_left = vect_left
+        self.vect_right = vect_right
+        self.qual_left = qual_left
+        self.qual_right = qual_right
+        self.clip_left = clip_left
+        self.clip_right = clip_right
+        self.tags = tags
+    
+    def __str__(self):
+        if self.ref_rc:
+            flag = 0x10 #maps to reverse strand
+            read_seq = reverse_complement(self.read_seq)
+            read_qual = self.read_qual[::-1]
+        else:
+            flag = 0
+            read_seq = self.read_seq
+            read_qual = self.read_qual
+        if not self.template_name:
+            assert self.read_name
+            self.template_name = self.read_name
+        elif self.template_name != self.read_name:
+            #Looks like a paired end read!
+            flag += 1
+            if self.first_in_pair:
+                flag += 0x40 #forward partner
+            else:
+                flag += 0x80 #reverse partner
+        assert not self.tags
+        mate_ref_name = -1
+        mate_ref_pos = -1
+        read_seq_unpadded = read_seq.replace("*", "")
+        read_qual_unpadded = "".join(q for (l,q) in zip(read_seq,read_qual) if l!="*")
+        cigar = self.cigar
+        if "D" not in cigar:
+            #Need deletions offset
+            if len(read_seq_unpadded) != sum(int(x) for x in cigar.replace("I","M").replace("S","M").split("M") if x):
+                raise ValueError("%s vs %i for %s" % (cigar, len(read_seq_unpadded), read_seq))
+        assert len(read_seq_unpadded) == len(read_qual_unpadded)
+        return "%s\t%i\t%s\t%i\t%i\t%s\t%s\t%i\t%s\t%s\t%s" % \
+            (self.template_name, flag, self.contig_name, self.ref_pos,
+             self.map_qual, cigar,
+             mate_ref_name, mate_ref_pos, self.insert_size,
+             read_seq_unpadded, read_qual_unpadded)
 
 print "@HD\tVN:1.0\tSO:unsorted"
 print "@CO\tConverted from a MIRA Alignment Format (MAF) file"
@@ -71,6 +135,8 @@ while True:
     assert contig_name in ref_lens
     padded_con_seq = None
     padded_con_qual = None
+    cached_pairs = dict()
+    current_read = None
 
     while True:
         line = maf_handle.readline()
@@ -100,56 +166,47 @@ while True:
                     index+=1
                 
             while line != "//\n":
-                read_name, template_name, read_seq, forward = "", "", "", True
-                flag, ref_pos, map_qual = 0, 0, 255
-                cigar = ""
-                mate_ref_name = "="
-                mate_ref_pos = 0
-                insert_size = 0
-                vect_left, vect_right = 0, 0
-                qual_left, qual_right = 0, 0
-                clip_left, clip_right = 0, 0
-                tags=""
+                current_read = Read(contig_name)
                 while True:
                     line = maf_handle.readline()
                     if line == "//\n":
                         break
                     elif line.startswith("RD\t"):
-                        read_name = line.rstrip().split("\t")[1]
-                        assert read_name
+                        current_read.read_name = line.rstrip().split("\t")[1]
+                        assert current_read.read_name
                     elif line.startswith("LR\t"):
                         #Optional length of read
                         pass
                     elif line.startswith("RS\t"):
-                        read_seq = line.rstrip().split("\t")[1]
+                        current_read.read_seq = line.rstrip().split("\t")[1]
                     elif line.startswith("RQ\t"):
-                        read_qual = line.rstrip().split("\t")[1]
-                        assert len(read_qual) == len(read_seq)
+                        current_read.read_qual = line.rstrip().split("\t")[1]
+                        assert len(current_read.read_qual) == len(current_read.read_seq)
                     elif line.startswith("SF\t"):
                         #name of seq file
                         pass
                     elif line.startswith("TN\t"):
-                        template_name = line.rstrip().split("\t")[1]
-                        assert read_name.startswith(template_name)
+                        current_read.template_name = line.rstrip().split("\t")[1]
+                        assert current_read.read_name.startswith(current_read.template_name)
                     elif line.startswith("DI\t"):
                         if line == "DI\tF\n":
-                            forward = True
+                            current_read.first_in_pair = True
                         elif line == "DI\tR\n":
-                            forward = False
+                            current_read.first_in_pair = False
                         else:
                             raise ValueError(line)
                     elif line.startswith("SL\t"):
-                        vect_left = int(line.rstrip().split("\t")[1])
+                        current_read.vect_left = int(line.rstrip().split("\t")[1])
                     elif line.startswith("SR\t"):
-                        vect_right = int(line.rstrip().split("\t")[1])
+                        current_read.vect_right = int(line.rstrip().split("\t")[1])
                     elif line.startswith("QL\t"):
-                        qual_left = int(line.rstrip().split("\t")[1])
+                        current_read.qual_left = int(line.rstrip().split("\t")[1])
                     elif line.startswith("QR\t"):
-                        qual_right = int(line.rstrip().split("\t")[1])
+                        current_read.qual_right = int(line.rstrip().split("\t")[1])
                     elif line.startswith("CL\t"):
-                        clip_left = int(line.rstrip().split("\t")[1])
+                        current_read.clip_left = int(line.rstrip().split("\t")[1])
                     elif line.startswith("CR\t"):
-                        clip_right = int(line.rstrip().split("\t")[1])
+                        current_read.clip_right = int(line.rstrip().split("\t")[1])
                     elif line.startswith("AO\t"):
                         #Align to Original... describes indels applied to the raw read
                         pass
@@ -178,27 +235,26 @@ while True:
                         #aligned to the contig. For the read positions, x2 is
                         #always < y2.
                         if x1 > y1:
-                            flag += 0x10 #maps to reverse strand
+                            current_read.ref_rc = True
                             #SAM stores these backwards:
                             cigar = make_cigar(padded_con_seq[y1-1:x1],
-                                               reverse_complement(read_seq[x2-1:y2]))
-                            read_qual = read_qual[::-1]
-                            read_seq = reverse_complement(read_seq)
+                                               reverse_complement(current_read.read_seq[x2-1:y2]))
                             #cigar = "%iM" % (x1-y1+1)
                             if x2 > 1:
                                 cigar += "%iS" % (x2-1)
-                            if y2 < len(read_seq):
-                                cigar = "%iS%s" % (len(read_seq)-y2, cigar)
+                            if y2 < len(current_read.read_seq):
+                                cigar = "%iS%s" % (len(current_read.read_seq)-y2, cigar)
                         else:
                             cigar = make_cigar(padded_con_seq[x1-1:y1],
-                                               read_seq[x2-1:y2])
+                                               current_read.read_seq[x2-1:y2])
                             #cigar = "%iM" % (y1-x1+1)
                             if x2 > 1:
                                 cigar = "%iS%s" % (x2-1, cigar)
-                            if y2 < len(read_seq):
-                                cigar += "%iS" % (len(read_seq)-y2)
-                        padded_pos = min(x1, y1)-1 #zero based
-                        ref_pos = mapping[padded_pos]+1 #one based for SAM
+                            if y2 < len(current_read.read_seq):
+                                cigar += "%iS" % (len(current_read.read_seq)-y2)
+                        current_read.cigar = cigar
+                        current_read.padded_pos = min(x1, y1)-1 #zero based
+                        current_read.ref_pos = mapping[current_read.padded_pos]+1 #one based for SAM
                         break #End of this read
                     elif not line:
                         raise ValueError("EOF in read")
@@ -206,32 +262,11 @@ while True:
                         raise ValueError("Bad line in read: %s" % repr(line))
                 if line == "//\n":
                     break
-                if not template_name:
-                    assert read_name
-                    template_name = read_name
-                elif template_name != read_name:
-                    #Looks like a paired end read!
-                    flag += 1
-                    if forward:
-                        flag += 0x40 #forward partner
-                    else:
-                        flag += 0x80 #reverse partner
-                assert not tags
-                read_seq_unpadded = read_seq.replace("*", "")
-                read_qual_unpadded = "".join(q for (l,q) in zip(read_seq,read_qual) if l!="*")
-                if "D" not in cigar:
-                    #Need deletions offset
-                    if len(read_seq_unpadded) != sum(int(x) for x in cigar.replace("I","M").replace("S","M").split("M") if x):
-                        raise ValueError("%s vs %i for %s" % (cigar, len(read_seq_unpadded), read_seq))
-                assert len(read_seq_unpadded) == len(read_qual_unpadded)
-                print "%s\t%i\t%s\t%i\t%i\t%s\t%s\t%i\t%s\t%s\t%s" % \
-                    (template_name, flag, contig_name, ref_pos, map_qual, cigar,
-                     mate_ref_name, mate_ref_pos, insert_size,
-                     read_seq_unpadded, read_qual_unpadded)
+                print current_read
         elif not line:
             raise ValueError("EOF in contig")
         else:
             raise ValueError("Bad line in contig: %s" % repr(line))
     #print contig_name, ref_lens[contig_name]
  
- 
+assert not cached_pairs, "%i left over paired reads" % len(cached_pairs)
